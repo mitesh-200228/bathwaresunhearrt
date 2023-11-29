b39358ef202abda591143de8d91317f6e82c043f
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2023-11-29 18:55:50 +0530

    latest one

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -90,8 +90,9 @@ export default function gridListWith() {
             />
           </Flex>
         </Container>
-        <Footer />
+
       </Box>
+        <Footer />
       {/* <Footer /> */}
     </>
   )
